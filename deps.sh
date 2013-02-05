7e368c4f7dc790b137f36c4de14034fe6c81421d
objectundefined <gabriel.lipson@gmail.com>
2013-02-05 00:46:31 -0800

    mods to get transcoder running in pepper 23

--- deps.sh
+++ deps.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 deps=(
-    libogg-1.1.4
-    libtheora-1.1.1
-    libvorbis-1.2.3
-    lame-398-2
-    ffmpeg-0.5
+#    libogg-1.1.4
+#    libtheora-1.1.1
+#    libvorbis-1.2.3
+#    lame-398-2
+#    ffmpeg-0.5
+    libogg
+    libtheora
+    libvorbis
+    lame
+    ffmpeg
 )
 
 for dep in ${deps[*]}; do
